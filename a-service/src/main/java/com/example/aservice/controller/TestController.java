cdaeb7af2c2b832ace01c0ab1f1b4fe98659692a
liaoyanan1 <1304418038@qq.com>
2020-07-03 14:55:29 +0800

    配置文件实时更新

--- a-service/src/main/java/com/example/aservice/controller/TestController.java
+++ a-service/src/main/java/com/example/aservice/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.aservice.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/getA")
 public class TestController {
 
+
+    @Value("${A}")
+    String A;
+
     @GetMapping("/A")
     public String A(){
-        return "A";
+        return A;
     }
 }

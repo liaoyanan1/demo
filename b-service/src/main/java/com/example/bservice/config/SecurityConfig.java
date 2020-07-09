91bd85591ece2f9391711e9ec9e741460b92b78c
liaoyanan1 <1304418038@qq.com>
2020-07-09 11:56:31 +0800

    修改路由和单点登录配置

--- b-service/src/main/java/com/example/bservice/config/SecurityConfig.java
+++ b-service/src/main/java/com/example/bservice/config/SecurityConfig.java
@@ -1,10 +1,9 @@
-package com.example.aservice.config;
+package com.example.bservice.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 
 
 @EnableOAuth2Sso
@@ -15,10 +14,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**")
                 .authorizeRequests()
-                .antMatchers( "/","/login")
+                .antMatchers("/", "/login")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
     }
-
 }
--- b-service/src/main/java/com/example/bservice/controller/TestController.java
+++ b-service/src/main/java/com/example/bservice/controller/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/getB")
 public class TestController {
 
     @GetMapping("/B")

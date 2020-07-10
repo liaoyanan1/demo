9328d29d2a89bdcc31861a5e28a92e0fa1104c59
liaoyanan1 <1304418038@qq.com>
2020-07-10 16:25:45 +0800

    eureka添加密码访问

--- discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
@@ -1,15 +1,26 @@
 package com.example.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
+
 @EnableEurekaServer
+@SpringBootApplication
 public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
-
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        }
+    }
 }
